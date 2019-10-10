03b9c43731ada3f0ae836d75b0baea8360b7c3bb
nabil <nabambil689@gmail.com>
2019-10-10 23:44:55 +0800

    rearrange code and homepage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
-import Header from './components/header.jsx';
+import Header from './components/header.js';
+import Home from './components/index.js';
+import Comparison from './components/comparison.js';
 
 function App() {
   return (
-    <div className="App">
+    <div id="wrapper">
+    {/* <div className="App"> */}
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p> Edit <code>src/App.js</code> and save to reload. </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> Learn React </a>
       </header> */}
+      <Comparison/>
       <Header />
+      <Home />
+    {/* </div> */}
     </div>
   );
 }
